45dd01c3ae0ef979b7bb776c69cafe14f22325c1
hrvth-dev <hrvth.dev@gmail.com>
2026-05-07 16:15:24 +0200

    DB hiba debugolás.

--- db/data.sql
+++ db/data.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE school_portal 
-CHARACTER SET utf8mb4 
+CREATE DATABASE school_portal
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_hungarian_ci;
 
 USE school_portal;
 
 
+-- Units tábla
+CREATE TABLE units (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+
+-- News tábla
 CREATE TABLE news (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -17,6 +25,7 @@ CREATE TABLE news (
 );
 
 
+-- Messages
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
@@ -26,33 +35,38 @@ CREATE TABLE messages (
 );
 
 
+-- Students
 CREATE TABLE students (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100),
-  class VARCHAR(20),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    class VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
+-- Uploads
 CREATE TABLE uploads (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  filename VARCHAR(255) NOT NULL,
-  original_name VARCHAR(255) NOT NULL,
-  file_type VARCHAR(50),
-  file_path VARCHAR(255) NOT NULL,
-  uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(255) NOT NULL,
+    original_name VARCHAR(255) NOT NULL,
+    file_type VARCHAR(50),
+    file_path VARCHAR(255) NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
+-- Users
 CREATE TABLE users (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(100) NOT NULL,
-  email VARCHAR(150) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role ENUM('user', 'admin') DEFAULT 'user',
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-) 
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') DEFAULT 'user',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
 
+
+-- Admin user
 INSERT INTO users (username, email, password, role)
 VALUES ('admin', 'admin@gmail.com', 'admin123', 'admin');
